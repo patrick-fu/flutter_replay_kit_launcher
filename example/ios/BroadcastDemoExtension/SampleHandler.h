64c027580b98427ff037b61d93c9815fb8c2326e
Patrick Fu <paaatrickfu@gmail.com>
2020-10-25 18:16:02 +0800

    feat: Add a simple demo app

---
+++ example/ios/BroadcastDemoExtension/SampleHandler.h
@@ -0,0 +1,12 @@
+//
+//  SampleHandler.h
+//  BroadcastDemoExtension
+//
+//  Created by Patrick Fu on 2020/10/25.
+//
+
+#import <ReplayKit/ReplayKit.h>
+
+@interface SampleHandler : RPBroadcastSampleHandler
+
+@end
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
@@ -14,32 +15,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await ReplayKitLauncher.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+  void launch() {
+    // Please fill in the name of the Broadcast Extension in your project, which is the file name of the `.appex` product
+    ReplayKitLauncher.launchReplayKitBroadcast('BroadcastDemoExtension');
   }
 
   @override
@@ -50,7 +34,40 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0),
+            child: Column(
+              children: [
+                Padding(padding: const EdgeInsets.only(top: 50.0)),
+                Text('Only support iOS (12.0 or above)'),
+                Padding(padding: const EdgeInsets.only(top: 20.0)),
+                Text('You should create a new `Broadcast Upload Extension` Target in your iOS `Runner` project first, and use this plugin to launch the Extension by `RPSystemBroadcastPickerView`',
+                  style: TextStyle(
+                    fontSize: 11.0,
+
+                  ),
+                ),
+                Padding(padding: const EdgeInsets.only(top: 20.0)),
+                Container(
+                  padding: const EdgeInsets.all(0.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Color(0xff0e88eb),
+                  ),
+                  width: 240.0,
+                  height: 60.0,
+                  child: CupertinoButton(
+                    child: Text('Launch ReplayKit',
+                      style: TextStyle(
+                        color: Colors.white
+                      ),
+                    ),
+                    onPressed: launch,
+                  ),
+                )
+              ],
+            )
+          )
         ),
       ),
     );
